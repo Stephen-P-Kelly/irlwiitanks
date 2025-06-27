a5825559e3b845b92721435cdc37c59adae59d39
Ben-T-Z <111848894+Ben-T-Z@users.noreply.github.com>
2025-06-26 21:52:08 -0600

    Update pi-setup.sh

--- pi-setup.sh
+++ pi-setup.sh
@@ -3,9 +3,7 @@
 sudo apt update && sudo apt upgrade -y
 
 sudo apt install -y git
-sudo apt install -y mosquitto mosquitto-clients
-
-sudo systemctl enable mosquitto
-sudo systemctl start mosquitto
+sudo apt install -y python3-pip
+pip3 install paho-mqtt
 
 echo "--- Done Installing! ---"
