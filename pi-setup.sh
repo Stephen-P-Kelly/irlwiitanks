e0312b05f4273004769cea8361459350b7d16681
Ben-T-Z <111848894+Ben-T-Z@users.noreply.github.com>
2025-06-26 21:54:09 -0600

    Update pi-setup.sh

--- pi-setup.sh
+++ pi-setup.sh
@@ -3,7 +3,6 @@
 sudo apt update && sudo apt upgrade -y
 
 sudo apt install -y git
-sudo apt install -y python3-pip
-pip3 install paho-mqtt
+sudo apt install -y python3-paho-mqtt
 
 echo "--- Done Installing! ---"
