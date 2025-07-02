e22c1cfc7e69174785a4ec8bd11c048caaa91244
Benjamin <vdm304@usask.ca>
2025-07-01 18:27:37 -0600

    Added more command methods and cleaned up a bit

--- pi_client4.py
+++ pi_client4.py
@@ -9,6 +9,8 @@ BROKER = "localhost"
 COMMAND_TOPIC = f"commands/{PI_ID}"
 RESPONSE_TOPIC = f"responses/{PI_ID}"
 
+lives = 3
+
 def on_connect(client, userdata, flags, rc):
     print(f"[{PI_ID.upper()}] Connected to broker.")
     client.subscribe(COMMAND_TOPIC)
@@ -21,19 +23,42 @@ def on_message(client, userdata, msg):
     print(f"[{PI_ID.upper()}] Received command: {command}")
     response = f"{PI_ID} received: {command}"
     client.publish(RESPONSE_TOPIC, response)
-    if "says:" in command:
-        #SimpleCommand = 'command'
-        SimpleCommand = command.split("says:", 1)[1].strip()
-        if SimpleCommand == "hit":
-            if lives > 0:
-                lives -= 1
-                print(f"[{PI_ID.upper()}] HIT! Lives remaining: {lives}")
-                if lives == 0:
-                    print(f"[{PI_ID.upper()}] DESTROYED!")
-            else:
-                print(f"[{PI_ID.upper()}] Already destroyed.")
-        else:
-            print(f"[{PI_ID.upper()}] Received non-hit command: {SimpleCommand}")
+    #SimpleCommand = 'command'
+    SimpleCommand = command.split("says:", 1)[1].strip()
+    handler = command_handlers.get(SimpleCommand)
+    if handler:
+        handler()
+    else:
+        print(f"[{PI_ID.upper()}] Unknown command: {SimpleCommand}")
+
+def start_message():
+    print(f"[{PI_ID.upper()}] Game starting in...")
+    for i in range(3, 0, -1):
+        print(f"  {i}...")
+        time.sleep(1)
+    print("  GO!\n")
+
+def hit_message():
+    global lives
+    if lives > 0:
+        lives -= 1
+        print(f"[{PI_ID.upper()}] HIT! Lives remaining: {lives}")
+        if lives == 0:
+            print(f"[{PI_ID.upper()}] DESTROYED!")
+    else:
+        print(f"[{PI_ID.upper()}] Already destroyed.")
+
+def reset_message():
+    global lives
+    lives = 3
+    print(f"[{PI_ID.upper()}] Lives reset to {lives}.")
+
+#Add all commands here
+command_handlers = {
+    "start": start_message,
+    "hit": hit_message,
+    "reset": reset_message
+}
 
 client = mqtt.Client(client_id=PI_ID)
 client.on_connect = on_connect
