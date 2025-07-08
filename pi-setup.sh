c27d3ffd5e94428d017857557d033b232108ddd5
stephenkelly <111624271+Stephen-P-Kelly@users.noreply.github.com>
2025-07-08 10:23:08 -0600

    Update pi-setup.sh

--- pi-setup.sh
+++ pi-setup.sh
@@ -6,6 +6,7 @@ sudo apt install -y git
 sudo apt install -y python3-paho-mqtt
 sudo apt install -y python3-picamera2
 sudo apt install -y python3-rpi.gpio
+sudo apt install -y python3-gpiozero
 
 echo "\n"
 echo "-=-=-=-=-=-=-=-=-=-=-=-="
