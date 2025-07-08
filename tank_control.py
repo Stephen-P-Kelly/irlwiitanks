9704ed1ed9d4949fad4c93c21db4b64c06e7ed6c
stephenkelly <111624271+Stephen-P-Kelly@users.noreply.github.com>
2025-07-08 14:19:19 -0600

    Update tank_control.py

--- tank_control.py
+++ tank_control.py
@@ -6,13 +6,15 @@
 # Imports #
 ###########
 from motor_driver_TB6612FNG import *
+from gpiozero import Servo
 from time import sleep
+from variables import *
 import socket as s
 import json
 
-#######################
-# Parameters / Config #
-#######################
+##############
+# Parameters #
+##############
 # Motor Driver
 AIN1 = x
 AIN2 = x
@@ -21,12 +23,20 @@ BIN1 = x
 BIN2 = x
 PWMB = x
 STBY = x
-#Motor(IN1,IN2,PWM,STANDBY,(Reverse polarity?))
-left_motor = DCMotor(AIN1, AIN2, PWMA, STBY, False)
-right_motor = DCMotor(BIN1, BIN2, PWMB, STBY, False)
 
 # Servo
 PWM = x
+
+##################
+# Initialization #
+##################
+
+def initialize():
+  left_motor = DCMotor(AIN1, AIN2, PWMA, STBY, False) # Motor(IN1,IN2,PWM,STANDBY,(Reverse polarity?))
+  right_motor = DCMotor(BIN1, BIN2, PWMB, STBY, False)
+  left_motor.standby(True)
+  right_motor.standby(True)
+  servo = Servo(PWM, initial_value=0)
 
 ########
 # Main #
@@ -45,6 +55,8 @@ If fired
   trigger firing logic
 '''
 
+initialize()
+
 # Setup UDP #
 tank_socket = s.socket(s.AF_INET, s.SOCK_DGRAM) # Create UDP socket
 tank_address = ('localhost', 12345) # Bind the socket to an address and port
@@ -52,20 +64,53 @@ tank_socket.bind(tank_address)
 print("UDP server is up and listening...")
 
 # Main control loop #
-while True:
-  data, controller_address = tank_socket.recvfrom(1024) # Receive data from client
-  print(f"Received message from {controller_address}: {data}")
-
-  try:
-    control_data = json.loads(data.decode('utf-8')) # Decode the JSON object
-    print("Decoded control data as JSON object:", control_data)
-  except json.JSONDecodeError as e:
-    print("Failed to decode control data as JSON object:", e)
-
-  # DC Motor Control #
+try:
+  while True:
+    # Recieve UDP packet from controller #
+    data, controller_address = tank_socket.recvfrom(1024) # Receive data from client
+    timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    print(f"[{timestamp}] Received message from {controller_address}: {data}")
   
+    try:
+      control_data = json.loads(data.decode('utf-8')) # Decode the JSON object
+      print("Decoded control data as JSON object:", control_data)
+    except json.JSONDecodeError as e:
+      print("Failed to decode control data as JSON object:", e)
 
-  # Servo Motor Control #
+    # Checking that the UDP packet was correctly transmitted
+    expected_keys = {"js_x", "js_y", "left", "right", "fire"}
+    if not expected_keys.issubset(control_data.keys()):
+      print("Missing one or more expected keys in control data")
+      continue
+    
+    # DC Motor Control #
+    # Motor speeds are between -100 and 100
+    base_speed = round(100 * ((control_data["js_y"] - CONTROLLER_ADC_MAX/2) / CONTROLLER_ADC_MAX)) # -100 to 100
+    turn_adjust = round(100 * ((control_data["js_x"] - CONTROLLER_ADC_MAX/2) / CONTROLLER_ADC_MAX)) # -100 to 100
+    
+    left_speed = max(-100, min(base_speed - turn_adjust, 100)) # Clamps speed between -100 and 100
+    right_speed = max(-100, min(base_speed + turn_adjust, 100)) # Clamps speed between -100 and 100
+    
+    left_motor.drive(left_speed) # drive left motor
+    right_motor.drive(right_speed) # drive right motor
   
-  
+    # Servo Motor Control #
+    # Servo angles (0 to 180 degrees) are mapped to values (-1 to 1)
+    if control_data["left"] != control_data["right"]:
+      if control_data["left"] == 1:
+        servo.value = max(servo.value - SERVO_INCREMENT, -1)
+      if control_data["right"] == 1:
+        servo.value = min(servo.value + SERVO_INCREMENT, 1)
+    else:
+      servo.detach()
+
+    # FIRE! #
+    if control_data["fire"] == 1:
+      # Do fire logic
+except KeyboardInterrupt:
+  print("\nShutting down tank controls...")
+  left_motor.standby(True)
+  right_motor.standby(True)
+  servo.detach()
+  tank_socket.close()
     
