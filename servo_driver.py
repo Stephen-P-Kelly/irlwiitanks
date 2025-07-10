c5369e24329000dc89daec511f28406cbf134757
stephenkelly <111624271+Stephen-P-Kelly@users.noreply.github.com>
2025-07-10 11:13:34 -0600

    Update servo_driver.py

--- servo_driver.py
+++ servo_driver.py
@@ -2,67 +2,67 @@
 import pigpio
 from random import random
 import time
+import math
 
 ''' 
 Converts the degrees for the servo to PWM input.
 It is unsafe for the servo to go below 500 us duty cycle or above
 2500 us duty cycle. This function keeps the servo within those bounds.
-  0 deg   -->  510 us
-  90 deg  -->  1500 us
-  180 deg -->  2490 us
+	0 deg   -->  510 us
+	90 deg  -->  1500 us
+	180 deg -->  2490 us
 '''
-deg2pwm = lambda deg: 510 + 11*deg
+deg2pwm = lambda ang: 510 + 11*ang
 
 
 '''
 Purpose:
-  This class is made to control the SG90 servo motors
+	This class is made to control the SG90 servo motors
 Info:
-  Servo motor angles are to be given as numbers between 0 and 180 (degrees)
+	Servo motor angles are to be given as numbers between 0 and 180 (degrees)
 '''
 class Servo:
-  pwm = ""
-	pwm_pin = ""
-	angle = ""
-
-	#Defaults
-	hertz = 50 # Hz
-  duty = 1000 # us
-
-	#Constructor
-	def __init__(self, pwm_pin, init_angle=90):
-    '''
-    Param:
-      pwm_pin - The GPIO pin for the PWM signal
-      init_angle - Initial angle for the servo
-    '''
+	def __init__(self, pwm_pin, init_angle=0, hertz=50):
+		'''
+		Param:
+		pwm_pin - The GPIO pin for the PWM signal
+		init_angle - Initial angle for the servo
+		'''
 		self.pwm_pin = pwm_pin
-    self.ang = init_angle
+		self.ang = init_angle
 
-    self.pwm = pigpio.pi()
-    self.pwm.set_mode(pwm_pin, pigpio.OUTPUT)
-    self.pwm.set_PWM_frequency(pwm_pin, 50)
-    self.pwm.set_servo_pulsewidth(pwm_pin, deg2pwm(self.ang))
+		self.pwm = pigpio.pi()
+		if not self.pwm.connected:
+		    raise RuntimeError("Failed to connect to pigpio daemon")
+		self.pwm.set_mode(self.pwm_pin, pigpio.OUTPUT)
+		self.pwm.set_PWM_frequency(self.pwm_pin, hertz)
+		self.pwm.set_servo_pulsewidth(self.pwm_pin, deg2pwm(self.ang))
 
-	def angle(self, degree):
-    if degree >= 0 and degree <= 180:
-      self.ang = degree
-  		self.pwm.set_servo_pulsewidth(pwm_pin, deg2pwm(self.ang))
-    else:
-      print("Servo is at max angle of " + str(self.ang) + "!")
+	def set_angle(self, ang):
+		self.ang = max(0, min(180, ang))
+		self.pwm.set_servo_pulsewidth(self.pwm_pin, deg2pwm(self.ang))
+
+	def get_angle(self):
+		return self.ang
 
 	def stop(self):
-		self.pwm.set_servo_pulsewidth(pwm_pin, 0)
-    
+		self.pwm.set_servo_pulsewidth(self.pwm_pin, 0)
+
 	def reset(self):
-		self.pwm.set_servo_pulsewidth(pwm_pin, 510)
+		self.pwm.set_servo_pulsewidth(self.pwm_pin, deg2pwm(0))
 
 	def dance(self):
-	  i = 0
-    while i < 10:
-      self.pwm.set_servo_pulsewidth(pwm_pin, deg2pwm(180*random()))
-      time.sleep(0.5)
-      i += 1
+		for i in range(10):
+			self.pwm.set_servo_pulsewidth(self.pwm_pin, deg2pwm(180*random()))
+			time.sleep(0.5)
 
-  def __del__(self):
-    self.pwm.stop()
+	def smooth_dance(self, duration=2, cycles=3, amplitude=90):
+	    start_time = time.time()
+	    while time.time() - start_time < duration:
+	        t = time.time()
+	        angle = 90 + amplitude * math.sin(2 * math.pi * cycles * t / duration)
+	        self.set_angle(angle)
+	        time.sleep(0.02)
+
+	def __del__(self):
+		self.pwm.stop()
