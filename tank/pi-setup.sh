46695a65647d84eec90d83758db61056ec8d9329
stephenkelly <111624271+Stephen-P-Kelly@users.noreply.github.com>
2025-07-18 14:14:57 -0600

    Cleaned up some files

--- tank/pi-setup.sh
+++ tank/pi-setup.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
+echo "-=-=-=-=-=-=-=-=-=-=-"
+echo "-=- Setting up Pi -=-"
+echo "-=-=-=-=-=-=-=-=-=-=-"
+echo "\n\n"
+
 sudo apt update && sudo apt upgrade -y
 
 sudo apt install -y git
-sudo apt install -y python3-paho-mqtt
-sudo apt install -y python3-picamera2
-sudo apt install -y python3-rpi.gpio
-sudo apt install -y python3-gpiozero
-sudo apt install -y python3-pigpio
+sudo apt install -y python3-paho-mqtt   # MQTT client
+sudo apt install -y python3-picamera2   # Camera module
+sudo apt install -y python3-rpi.gpio    # Used in DC motor driver
+sudo apt install -y python3-pigpio      # Used in servo motor driver
 
-echo "\n"
-echo "-=-=-=-=-=-=-=-=-=-=-=-="
-echo "-=- Done Installing! -=-"
-echo "-=-=-=-=-=-=-=-=-=-=-=-="
+echo "\n\n"
+echo "-=-=-=-=-=-=-=-=-=-=-=-"
+echo "-=- Done Setting Up -=-"
+echo "-=-=-=-=-=-=-=-=-=-=-=-"
